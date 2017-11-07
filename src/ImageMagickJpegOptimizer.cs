cb8b7b035c353eceb58e76771cd4c3bb04456804
John Rappel <jrappel@geekhive.com>
2017-11-07 09:33:37 -0600

    Major improvements over reduction of image sizes.

--- src/ImageMagickJpegOptimizer.cs
+++ src/ImageMagickJpegOptimizer.cs
@@ -4,26 +4,34 @@ namespace Dianoga.ImageMagick
 {
     public class ImageMagickJpegOptimizer : CommandLineToolOptimizer
     {
-        private static int Quality => Sitecore.Configuration.Settings.GetIntSetting("Media.Resizing.Quality", 72);
-
         private static int MaxWidth => Sitecore.Configuration.Settings.GetIntSetting("Media.Resizing.MaxWidth", 2000);
         private static int MaxHeight => Sitecore.Configuration.Settings.GetIntSetting("Media.Resizing.MaxHeight", 1000);
-
+        
+        private string _additionalImageMagick;
+        
+        public virtual string AdditionalImageMagick
+        {
+            get => _additionalImageMagick;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    return;
+                _additionalImageMagick = value;
+            }
+        }
+        
         protected override string CreateToolArguments(string tempFilePath, string tempOutputPath)
         {
-            if (Quality <= 0)
-                return string.Empty;
-
             if(MaxWidth > 0 && MaxHeight > 0)
-                return $"convert \"{tempFilePath}\" -resize x{MaxHeight}^> -resize {MaxWidth}^> -quality {Quality} \"{tempOutputPath}\"";
+                return $"convert \"{tempFilePath}\" {AdditionalImageMagick} -resize x{MaxHeight}^> -resize {MaxWidth}^> \"{tempOutputPath}\"";
             
             if (MaxWidth > 0)
-                return $"convert \"{tempFilePath}\" -resize {MaxWidth}> -quality {Quality} \"{tempOutputPath}\"";
+                return $"convert \"{tempFilePath}\" {AdditionalImageMagick} -resize {MaxWidth}> \"{tempOutputPath}\"";
 
             if (MaxHeight > 0)
-                return $"convert \"{tempFilePath}\" -resize x{MaxHeight}> -quality {Quality} \"{tempOutputPath}\"";
+                return $"convert \"{tempFilePath}\" {AdditionalImageMagick} -resize x{MaxHeight}> \"{tempOutputPath}\"";
 
-            return $"convert \"{tempFilePath}\" -quality {Quality} \"{tempOutputPath}\"";
+            return "";
         }
     }
 }
